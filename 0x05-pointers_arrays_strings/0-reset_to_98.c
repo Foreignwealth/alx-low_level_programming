4d41051d27739c8839dd1df830ef0e0bb2d93db8
Foreignwealth <opeyemioladoyinbo01@gmail.com>
2022-12-27 13:17:37 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,18 +1,12 @@
 #include "main.h"
 /**
- * reset_to_98 - updates the value it points to
- * @n: is an integer
- * @*p - is a pointer
- * Return: 0
+ *  reset_to_98 - updates the value it points to
+ *  @n: is an integer
+ *  @*p - is a pointer
+ *  Return: 0
  */
 
 void reset_to_98(int *n)
 {
-
-int n;
-int *p = &n;
-_putchar("n=%d\n", n);
-
-*p = 98;
-_putchar("n=%d\n", n);
+*n = 98;
 }
