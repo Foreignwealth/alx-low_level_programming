5c345354314c6e4abeaf9590686a6f87fffe5401
Foreignwealth <opeyemioladoyinbo01@gmail.com>
2023-01-01 10:39:17 -0800

    10-add.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
 void print_alphabet(void);
 int _putchar(char);
 void print_alphabet_x10(void);
@@ -10,5 +7,4 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void times_table(void);
-#endif
+void jack_bauer(void);
