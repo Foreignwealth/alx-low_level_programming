47eb6cb6e19b30a2a0371c592959245f6c68d056
Foreignwealth <opeyemioladoyinbo01@gmail.com>
2023-01-07 16:13:26 -0800

    libmy.a, main.h

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints string followed by a new line
+ * @str: string to be printed
+ * Return: 0
+ */
+void _puts(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
